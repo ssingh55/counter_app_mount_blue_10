3accf05b15b810daeca99f34438684924cc562e1
Shubham Singh <shubham.singh@mountblue.io>
2018-07-28 15:41:12 +0530

    Simple counter application using react done

--- src/counterAppln.js
+++ src/counterAppln.js
@@ -1,2 +1,34 @@
 import React, { Component } from 'react';
 
+export default class Counter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+    increment = () => {
+        this.setState((prevState) => ({
+            count: prevState.count + 1
+        }))
+    }
+    decrement = () => {
+        var n = this.state.count;
+        n--;
+        this.setState(
+            {
+                count: n
+            }
+        )
+    }
+    render() {
+        return (
+            <div className='counter'>
+                <h1>Counter using react</h1>
+                <button onClick={this.decrement}>-</button>
+                <p>{this.state.count}</p>
+                <button onClick={this.increment}>+</button>
+            </div>
+        )
+    }
+}
