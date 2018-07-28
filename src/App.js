33eaf004089d3d3173674a09dd1d5f8596dcc873
Shubham Singh <shubham.singh@mountblue.io>
2018-07-28 17:43:15 +0530

    Counter appln using react and redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 import Counter from './counterAppln';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const initialState = {
+  count: 0
+}
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT": {
+      return {
+        count: state.count - 1
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+// store.dispatch({
+//   type: 'INCREMENT'
+// })
+// store.dispatch({
+//   type: 'DECREMENT'
+// })
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Provider store={store}>
         <Counter />
-      </div>
+      </Provider>
     );
   }
 }
--- src/counterAppln.js
+++ src/counterAppln.js
@@ -1,34 +1,30 @@
 import React, { Component } from 'react';
-
-export default class Counter extends Component {
+import { connect } from 'react-redux';
+class Counter extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            count: 0
-        }
+        
     }
     increment = () => {
-        this.setState((prevState) => ({
-            count: prevState.count + 1
-        }))
+        this.props.dispatch({type: "INCREMENT"})
     }
     decrement = () => {
-        var n = this.state.count;
-        n--;
-        this.setState(
-            {
-                count: n
-            }
-        )
+        this.props.dispatch({type: "DECREMENT"})
     }
     render() {
         return (
             <div className='counter'>
                 <h1>Counter using react</h1>
                 <button onClick={this.decrement}>-</button>
-                <p>{this.state.count}</p>
+                <p>{this.props.count}</p>
                 <button onClick={this.increment}>+</button>
             </div>
         )
     }
 }
+
+const mapStateToProps = state => ({
+    count: state.count
+})
+
+export default connect(mapStateToProps)(Counter);
